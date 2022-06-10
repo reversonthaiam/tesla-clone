59f9a2a85c0fd0fe8ff876327c069ccb0f19f542
Reverson Thaiam <reverson.santos@frotabank.com.br>
2022-06-10 10:48:54 -0300

    Tesla clone start

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Container, HeaderImg, HeaderLogo, HeaderLinks } from './styles'
+
+function Header() {
+  return (
+    <Container>
+      <HeaderLogo>
+        <Link to="/">
+          <HeaderImg
+            src="https://assets.website-files.com/5e8fceb1c9af5c3915ec97a0/5ec2f037975ed372da9f6286_Tesla-Logo-PNG-HD.png"
+            alt="Logo"
+          />
+        </Link>
+      </HeaderLogo>
+      <HeaderLinks>
+        <Link to='/'>Modal S</Link>
+        <Link to='/'>Modal 3</Link>
+        <Link to='/'>Modal X</Link>
+        <Link to='/'>Modal Y</Link>
+        <Link to='/'>Solar Roof</Link>
+        <Link to='/'>Solar Panels</Link>
+      </HeaderLinks>
+      
+    </Container>
+  )
+}
+
+export default Header
---
+++ src/Components/Header/styles.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  display: flex;
+  position: sticky;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  right: 0;
+  justify-content: space-between;
+  align-items: center;
+  padding: 25px;
+  padding-top: 15px;
+`
+
+export const HeaderLogo = styled.div``
+
+export const HeaderImg = styled.img`
+  object-fit: contain;
+  flex: 1;
+  width: 90px;
+  margin-right: 100px;
+`
+export const HeaderLinks = styled.div`
+
+`
